074ae40769a8890ab6ce8c232759d856d63dea47
Jason <git-jason@bodleum.xyz>
2021-06-02 12:51:33 +0000

    Moved up to html dir in jason

---
+++ test/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!--
+	<meta http-equiv="refresh" content="0, url='Bodleum/index.html'" />
+	-->
+</head>
+
+<body>
+	<p>Please follow <a href="Bodleum/index.html">this</a>link.</p>
+	<p>Hello</p>
+</body>
+</html>
