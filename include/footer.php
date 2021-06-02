c64b343185e2f4dc2bed2f657102e84384fafccc
Bodleum <bodleum.phone@gmail.com>
2021-06-02 12:31:05 +0100

    PHP for real

---
+++ include/footer.php
@@ -0,0 +1,35 @@
+			<div id="footer", class="cf">
+
+				<div class="columnThree">
+					<strong>Phone</strong>
+					808.529.3819
+				</div> <!-- columnThree -->
+
+				<div class="columnThreeLast">
+
+					<strong>Hours</strong>
+					<em>Tuesday - Thursday</em><br>
+					1:00pm - 9:00pm<br><br>
+
+					<em>Friday - Saturday</em><br>
+					4:00pm - 11:00pm<br><br>
+
+					<em>Sunday - Monday</em><br>
+					Closed<br><br>
+
+				</div> <!-- columnThreeLast -->
+
+			</div> <!-- footer -->
+
+			<small>&copy;2021 Daniel, Jude Laing</small>
+
+		</div> <!-- content -->
+
+	</div> <!-- wrapper -->
+
+	<div class="copyrightInfo">
+		<p>Copyright</p>
+	</div> <!-- copyrightInfo -->
+
+</body>
+</html>
