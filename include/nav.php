52516e095323c96582f17acdfeb4fe9ffb7955db
Bodleum <bodleum.phone@gmail.com>
2021-06-02 12:52:48 +0100

    Changed index to php file

--- include/nav.php
+++ include/nav.php
@@ -1,5 +1,5 @@
 <ul>
-	<li><a href="index.html">Home</a></li>
+	<li><a href="index.php">Home</a></li>
 	<li><a href="findDonkey.html">Donkey</a></li>
 	<li><a href="adventCalendar.html">Calendar</a></li>
 </ul>
